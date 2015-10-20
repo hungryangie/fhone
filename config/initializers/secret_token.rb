65cff31dee2e67dedbdeea011ccb13476732332f
Angelica <angelica.hom@gmail.com>
2015-10-20 04:44:41 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fhone::Application.config.secret_key_base = 'e4da97068a4a28cf4b8ff5737ce0e98ecd89ab4ecee4f8c8c045173ef1eca2b9f4756edafdb50d91f44b664f7ca08b0c59a42578557af9a2d3aaf0660bdb3fac'
